479ef30b9215339e634aab90ee7686d0a13012c7
Jessica Kim <jessica.kim@outlook.de>
2022-03-28 22:34:33 +0200

    func

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -181,3 +181,12 @@ func showWelcome() {
     print("chart from centimeters to inches, but you")
     print("can also set a custom range if you want.")
 }
+
+
+func printTimesTables(number: Int, end: Int) {
+    for i in 1...end {
+        print("\(i) x \(number) is \(i * number)")
+    }
+}
+
+printTimesTables(number: 5, end: 20)
