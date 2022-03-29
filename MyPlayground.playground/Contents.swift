6a6f8e9189933a52c1ff54fd29ec3fb46356485c
Jessica Kim <jessica.kim@outlook.de>
2022-03-29 22:19:50 +0200

    Basics SwiftUI

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -190,3 +190,38 @@ func printTimesTables(number: Int, end: Int) {
 }
 
 printTimesTables(number: 5, end: 20)
+
+func getUser() -> [String] {
+    ["Taylor", "Swift"]
+}
+
+//Tuple
+
+let user = getUser()
+print("Name: \(user[0]) \(user[1])")
+
+func rollDice(sides: Int, count: Int) -> [Int] {
+    // Start with an empty array
+    var rolls = [Int]()
+
+    // Roll as many dice as needed
+    for _ in 1...count {
+        // Add each result to our array
+        let roll = Int.random(in: 1...sides)
+        rolls.append(roll)
+    }
+
+    // Send back all the rolls
+    return rolls
+}
+
+let rolls = rollDice(sides: 6, count: 4)
+
+
+func printTimesTables(for number: Int) {
+    for i in 1...12 {
+        print("\(i) x \(number) is \(i * number)")
+    }
+}
+
+printTimesTables(for: 5)
